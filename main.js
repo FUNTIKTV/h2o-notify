c04e5a90c2cc9d3837301c1c391ad5264ffd4c36
FUNTIKTV <122114314+FUNTIKTV@users.noreply.github.com>
2025-07-05 19:26:20 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,21 @@ let water = 0;
 let target = localStorage.getItem("target") || 3000;
 
 function updateUI() {
-  const percent = Math.round((water / target) * 100);
-  document.getElementById("progress").textContent = `${percent}%`;
+  let start = parseInt(document.getElementById("progress").textContent) || 0;
+  const end = Math.round((water / target) * 100);
+  const step = end > start ? 1 : -1;
+
+  let current = start;
+  const interval = setInterval(() => {
+    current += step;
+    document.getElementById("progress").textContent = `${current}%`;
+    if (current === end) clearInterval(interval);
+  }, 10); // швидкість анімації
+
   document.getElementById("volume").textContent = `${water} мл із ${target} мл`;
   document.getElementById("target").value = target;
 }
+
 
 function addWater(amount) {
   water += amount;
